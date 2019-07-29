5e81470bbeb2c005efd5b8029f4d4a25ce2de9d6
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-07-29 16:35:09 +0530

    File updated

--- Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
    
+	@Autowired
+	private MessageSource messageSource;
 	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world") or
 	@GetMapping(path = "/hello-world")
 	public String helloWorld(){
@@ -24,5 +32,16 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	/*
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+		return messageSource.getMessage("good.morning.message", null , locale);
+	}*/
+	
+	//Alternate Way to above method for internationalization
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(){
+		return messageSource.getMessage("good.morning.message", null ,LocaleContextHolder.getLocale());
 	}
 }
