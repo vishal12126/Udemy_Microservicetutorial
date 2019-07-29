9eeaf9e3769fb3608295b90bb7f40fb9d876eb66
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-07-29 16:39:11 +0530

    File updated

--- Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,15 @@
 package com.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +18,19 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		//SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		//For using LocaleContextHolder we need to use AcceptLocaleResolver insteas of Session
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
