87f918961499a62f3761d6e038b772a10e87f154
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-08-01 10:38:11 +0530

    Added static filetring using @JsonIgnore

---
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.restfulwebservices.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean(){
+		return new SomeBean("values1","value2","values3");
+	}
+}
