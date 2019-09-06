4a539d4db62d26e54e24f5a97021f4fc17b3b17b
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-09-06 09:31:43 +0530

    Added different types of versioning example

--- Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -15,4 +15,37 @@ public class PersonVersioningController {
 	public PersonV2 personV2(){
 		return new PersonV2(new Name("vishal", "Singh"));
 	}
+	
+	//Using params for versioning
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Vishal Hello world");
+	}
+	
+	@GetMapping(value="/person/param",params="version=2")
+	public PersonV2 paramV2(){
+		return new PersonV2(new Name("vishal hello", "Singh"));
+	}
+	
+	//Using header parameter
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Vishal Hello world");
+	}
+	
+	@GetMapping(value="/person/header",headers="X-API-VERSION=2")
+	public PersonV2 headerV2(){
+		return new PersonV2(new Name("vishal hello", "Singh"));
+	}
+	
+	//Using produces for versioning
+	@GetMapping(value="/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Vishal Hello world");
+	}
+	
+	@GetMapping(value="/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2(){
+		return new PersonV2(new Name("vishal hello", "Singh"));
+	}
 }
