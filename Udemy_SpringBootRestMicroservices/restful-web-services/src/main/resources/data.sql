8d9d9315e04997bda4241822e3ae3bf909a456b9
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-09-06 13:54:32 +0530

    Added JPA and using H2 in mem DB

---
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values (1,sysdate(),'vishal');
+insert into user values (2,sysdate(),'Harpreet');
+insert into user values (3,sysdate(),'jack');
+insert into user values (4,sysdate(),'shukla');
