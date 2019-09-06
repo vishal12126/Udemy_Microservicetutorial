b980a527f9e00debaad3bed3d13353cf62948b04
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-09-06 14:35:56 +0530

    Added CRUD operation to UserJpaResource

--- Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -1,13 +1,21 @@
 package com.rest.webservices.restfulwebservices.user;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserJPAResource {
@@ -31,5 +39,18 @@ public class UserJPAResource {
 		}
 		return user;
 		
+	}
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		repository.deleteById(id);
+	}
+	
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+		User savedUser = repository.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{Id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
