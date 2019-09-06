efec7f6b43f9e6698fc22fe1379d9521d0886052
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-09-06 13:55:52 +0530

    Added JPA basic annotations in USER.java

--- Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ Udemy_SpringBootRestMicroservices/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,20 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "ALL details about User")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	@Size(min=2, message="Name should have atleast 2 characters")
 	@ApiModelProperty(notes="Name should have atleast 2 characters")
